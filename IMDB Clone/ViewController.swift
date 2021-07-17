81d836df4a255db7d4bc561c610dc616e478aa20
Konstantin Kostadinov <lilkosi@Konstantins-MacBook-Pro.local>
2021-07-17 13:55:21 +0300

    initial commit

---
+++ IMDB Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDB Clone
+//
+//  Created by Konstantin Kostadinov on 17.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
