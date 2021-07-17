a7df9636c9b93a9c4c911eb259c5dbff03023f51
Konstantin Kostadinov <lilkosi@Konstantins-MacBook-Pro.local>
2021-07-17 15:37:16 +0300

    Added kingfisher. Presenting fetched data/

---
+++ IMDB Clone/Views/Top250Movies/TopMovieTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TopMovieTableViewCell.swift
+//  IMDB Clone
+//
+//  Created by Konstantin Kostadinov on 17.07.21.
+//
+
+import UIKit
+
+class TopMovieTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingAndYearLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
